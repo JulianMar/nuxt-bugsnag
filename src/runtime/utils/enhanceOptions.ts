5d7977716a0aadd9c8e858209a2ac909916a6a72
Julian Martin <julian.martin@russmedia.com>
2025-08-08 12:57:12 +0200

    fix: switch onError to be an array of functions instead of overriding it.

--- src/runtime/utils/enhanceOptions.ts
+++ src/runtime/utils/enhanceOptions.ts
@@ -6,12 +6,20 @@ export default function enhanceOptions<T extends BrowserConfig | NodeConfig>(mod
   const options: T = { ...moduleOptions }
 
   options.plugins = [...options.plugins || [], new BugsnagPluginVue()]
-  options.onError = (event) => {
+
+  options.onError = options.onError || []
+
+  if (typeof options.onError === 'function') {
+    const originalOnError = options.onError
+    options.onError = [originalOnError]
+  };
+
+  options.onError.push((event) => {
     event.errors[0].stacktrace = event.errors[0].stacktrace.map((row) => {
       row.file = row.file.replace('file://', '')
       return row
     })
-  }
+  })
 
   return options
 }
